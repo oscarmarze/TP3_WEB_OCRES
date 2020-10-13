4b78688919e14bdd2b89462e66abaee3ae98fa96
oscarmarze <oscar.marze@edu.ece.fr>
2020-10-13 11:04:41 +0200

    FINI

--- js/script.js
+++ js/script.js
@@ -72,17 +72,15 @@ function getThreeDayForecast(){
         
         const main = data.list[i].weather[0].main;
         const description = data.list[i].weather[0].description;
-        const temp = data.list[i].main.temp;
-        window.alert("test5");
+        const temp = data.list[i].temp.day;
         const icon = apiWeather.getHTMLElementFromIcon(data.list[i].weather[0].icon);
-        window.alert("test2");
         if(i==1)
         {
           document.getElementById('tomorrow-forecast-main').innerHTML = main;
           document.getElementById('tomorrow-forecast-more-info').innerHTML = description;
           document.getElementById('tomorrowicon-weather-container').innerHTML = icon;
           document.getElementById('tomorrow-forecast-temp').innerHTML = `${temp}°C`;
-          window.alert("1");
+         
         }
 
         else if(i==2)
@@ -109,7 +107,53 @@ function getThreeDayForecast(){
       // Affiche une erreur
       console.error(error);
     });
-  
-
 }
+function ActualisegetThreeDayForecast(){
+  var city = document.getElementById('city-input').value;
+  const apiWeather = new API_WEATHER(city);
+  apiWeather
+    .fetchThreeDayForecast()
+    .then(function(response) {
+      // Récupère la donnée d'une API
+      const data = response.data;
+      for(let i=1;i<4;i++) // la boucle for de 1 à 3 permet de parcourir les jour voulu, 0 étant ajrd
+      {
+        
+        const main = data.list[i].weather[0].main;
+        const description = data.list[i].weather[0].description;
+        const temp = data.list[i].temp.day;
+        const icon = apiWeather.getHTMLElementFromIcon(data.list[i].weather[0].icon);
+        if(i==1)
+        {
+          document.getElementById('tomorrow-forecast-main').innerHTML = main;
+          document.getElementById('tomorrow-forecast-more-info').innerHTML = description;
+          document.getElementById('tomorrowicon-weather-container').innerHTML = icon;
+          document.getElementById('tomorrow-forecast-temp').innerHTML = `${temp}°C`;
+         
+        }
+
+        else if(i==2)
+        {
+          document.getElementById('tomorrow2-forecast-main').innerHTML = main;
+          document.getElementById('tomorrow2-forecast-more-info').innerHTML = description;
+          document.getElementById('tomorrow2icon-weather-container').innerHTML = icon;
+          document.getElementById('tomorrow2-forecast-temp').innerHTML = `${temp}°C`;
+        }
+
+        else if(i==3)
+        {
+          document.getElementById('tomorrow3-forecast-main').innerHTML = main;
+          document.getElementById('tomorrow3-forecast-more-info').innerHTML = description;
+          document.getElementById('tomorrow3icon-weather-container').innerHTML = icon;
+          document.getElementById('tomorrow3-forecast-temp').innerHTML = `${temp}°C`;
+        }
+
+      }
+      
+      
+    })
+    .catch(function(error) {
+      // Affiche une erreur
+      console.error(error);
+    });}
 
